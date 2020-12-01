0ce78fb68297d41da039c9da51dff918a1e3772d
Arnold Andreasson <arnold@mellifica.se>
2020-12-01 15:34:25 +0100

    Command line interface, CLI, added.

---
+++ dwca_generator_cli.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+#
+# Copyright (c) 2020-present SMHI, Swedish Meteorological and Hydrological Institute
+# License: MIT License (see LICENSE.txt or http://opensource.org/licenses/mit).
+
+import pathlib
+import click
+import dwca_generator_main
+
+global dwca_configs
+
+
+@click.command()
+@click.option(
+    "--row",
+    default=0,
+    prompt="Execute row",
+    help="Row number used to select which YAML-file to generate DwC-A from.",
+)
+def run_dwca_generator_command(row):
+    """ """
+    global dwca_configs
+    if (row < 0) or (row > len(dwca_configs)):
+        print("\n\nERROR: Wrong value. Please try again.\n\n")
+        return
+    if row == 0:
+        for config_file in dwca_configs:
+            dwca_generator_main.generate_dwca(config_file)
+    else:
+        dwca_generator_main.generate_dwca(dwca_configs[row - 1])
+
+
+if __name__ == "__main__":
+    """ """
+    global dwca_configs
+    dwca_configs = []
+    for file_path in pathlib.Path("dwca_config").glob("dwca_*.yaml"):
+        dwca_configs.append(str(file_path))
+    dwca_configs = sorted(dwca_configs)
+    # Print before command.
+    print("\n\nDarwinCore-Archive generator.")
+    print("-----------------------------")
+    print("Select row number. Press enter to run all.")
+    print("Press Ctrl-C to terminate.\n")
+    for index, row in enumerate(dwca_configs):
+        print(index + 1, "  ", row)
+    print("")
+    # Execute command.
+    run_dwca_generator_command()
--- dwca_generator_main.py
+++ dwca_generator_main.py
@@ -6,51 +6,57 @@
 
 import dwca_generator
 
+
+def generate_dwca(config_file):
+    """ """
+    print("\n\n\n=== Processing: ", config_file)
+
+    # Config and EML content.
+    dwca_gen_config = dwca_generator.DwcaGeneratorConfig(config_file)
+    dwca_gen_config.load_config()
+    eml_content_rows = dwca_gen_config.generate_eml_content()
+
+    # Prepare data.
+    print("\n=== Preparing data ===")
+    source_data = dwca_generator.DwcaDataSharkStandard(dwca_gen_config)
+    for dataset_filepath in dwca_gen_config.source_files:
+        source_data.add_shark_dataset(dataset_filepath)
+    source_data.create_dwca_keys()
+    source_data.cleanup_data()
+
+    # Create and save DwC-A.
+    print("\n=== Creating DwC-A ===")
+    species_info = dwca_generator.DwcaSpeciesWorms(
+        taxa_file_path=dwca_gen_config.taxa_worms_file
+    )
+    dwca_format = dwca_generator.DwcaFormatStandard(
+        source_data, dwca_gen_config, species_info
+    )
+    dwca_format.create_dwca_event()
+    dwca_format.create_dwca_occurrence()
+    dwca_format.create_dwca_measurementorfact()
+    dwca_format.extract_metadata()
+    dwca_format.add_metadata_to_eml(eml_content_rows)
+    dwca_format.create_meta_xml()
+    dwca_format.save_to_archive_file()
+
+    # Print missing taxa.
+    missing_taxa_list = species_info.get_missing_taxa_list()
+    if len(missing_taxa_list) > 0:
+        print("\n   Missing taxa: ")
+        for taxa in missing_taxa_list:
+            print("   - ", taxa)
+
+    print("\n=== Finished: ", config_file)
+
+
 if __name__ == "__main__":
     """ """
-
+    # Test configs.
     config_files = [
         "dwca_config/dwca_bacterioplankton_nat.yaml",
-        "dwca_config/dwca_zooplankton_nat.yaml",
-        "dwca_config/dwca_zoobenthos_nat.yaml",
+        # "dwca_config/dwca_zooplankton_nat.yaml",
+        # "dwca_config/dwca_zoobenthos_nat.yaml",
     ]
-
     for config_file in config_files:
-
-        print("\n\n\n=== Processing: ", config_file)
-
-        # Config and EML content.
-        dwca_gen_config = dwca_generator.DwcaGeneratorConfig(config_file)
-        dwca_gen_config.load_config()
-        eml_content_rows = dwca_gen_config.generate_eml_content()
-
-        # Prepare data.
-        print("\n=== Preparing data ===")
-        source_data = dwca_generator.DwcaDataSharkStandard(dwca_gen_config)
-        for dataset_filepath in dwca_gen_config.source_files:
-            source_data.add_shark_dataset(dataset_filepath)
-        source_data.create_dwca_keys()
-        source_data.cleanup_data()
-
-        # Create and save DwC-A.
-        print("\n=== Creating DwC-A ===")
-        species_info = dwca_generator.DwcaSpeciesWorms(
-            taxa_file_path=dwca_gen_config.taxa_worms_file
-        )
-        dwca_format = dwca_generator.DwcaFormatStandard(
-            source_data, dwca_gen_config, species_info
-        )
-        dwca_format.create_dwca_parts()
-        dwca_format.extract_metadata()
-        dwca_format.add_metadata_to_eml(eml_content_rows)
-        dwca_format.create_meta_xml()
-        dwca_format.save_to_archive_file()
-
-        # Print missing taxa.
-        missing_taxa_list = species_info.get_missing_taxa_list()
-        if len(missing_taxa_list) > 0:
-            print("\n   Missing taxa: ")
-            for taxa in missing_taxa_list:
-                print("   - ", taxa)
-
-        print("\n=== Finished ===")
+        generate_dwca(config_file)
